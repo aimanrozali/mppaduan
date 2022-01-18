1f9517bf81096826a250ff6471cbe3a6127c0bc2
aimanrozali <aimanrozali@student.usm.my>
2022-01-19 02:33:07 +0800

    Change Server Connection

--- create_conn.php
+++ create_conn.php
@@ -1,15 +1,17 @@
 <?php
 
-$servername = "mppaduan.database.windows.net";
-$username = "adminkacak";
-$password = "Kacak_gagah01";
-$dbname = "mppaduan";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error) . "<br>";
+// PHP Data Objects(PDO) Sample Code:
+try {
+  $conn = new PDO("sqlsrv:server = tcp:mppaduan.database.windows.net,1433; Database = mppaduan", "adminkacak", "Kacak_gagah01");
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
+catch (PDOException $e) {
+  print("Error connecting to SQL Server.");
+  die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "adminkacak", "pwd" => "Kacak_gagah01", "Database" => "mppaduan", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:mppaduan.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
