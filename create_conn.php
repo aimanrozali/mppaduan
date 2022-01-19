507973ffc7dc39b3723ccea508576b1edfe3bdd0
aimanrozali <aimanrozali@student.usm.my>
2022-01-20 07:41:40 +0800

    updated connection

--- create_conn.php
+++ create_conn.php
@@ -1,19 +1,19 @@
 <?php
 
-// $servername = "aduanmpp.mysql.database.azure.com";
-// $username = "adminkacak@aduanmpp";
-// $password = "Kacak_gagah01";
-// $dbname = "aduan";
+$servername = "aduanmpp.mysql.database.azure.com";
+$username = "adminkacak@aduanmpp";
+$password = "Kacak_gagah01";
+$dbname = "aduan";
 
-// // Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// // Check connection
-// if ($conn->connect_error) {
-//   die("Connection failed: " . $conn->connect_error) . "<br>";
-// }
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error) . "<br>";
+}
 
-$conn=mysqli_init(); 
-mysqli_ssl_set($conn, NULL, NULL, {ca-cert filename}, NULL, NULL); 
-mysqli_real_connect($conn, "aduanmpp.mysql.database.azure.com", "adminkacak@aduanmpp", "Kacak_gagah01", "aduan", 3306);
+// $conn=mysqli_init(); 
+// mysqli_ssl_set($conn, NULL, NULL, {ca-cert filename}, NULL, NULL); 
+// mysqli_real_connect($conn, "aduanmpp.mysql.database.azure.com", "adminkacak@aduanmpp", "Kacak_gagah01", "aduan", 3306);
 ?>
