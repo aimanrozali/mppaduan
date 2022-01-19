6ed2f665e10ab2ed66c4da4c52c36ab08f979e43
aimanrozali <aimanrozali@student.usm.my>
2022-01-20 07:25:39 +0800

    updated connection

--- create_conn.php
+++ create_conn.php
@@ -1,17 +1,6 @@
 <?php
 
-// PHP Data Objects(PDO) Sample Code:
-try {
-  $conn = new PDO("sqlsrv:server = tcp:mppaduan.database.windows.net,1433; Database = mppaduan", "adminkacak", "Kacak_gagah01");
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-  print("Error connecting to SQL Server.");
-  die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "adminkacak", "pwd" => "Kacak_gagah01", "Database" => "mppaduan", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:mppaduan.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+$con=mysqli_init(); 
+mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
+mysqli_real_connect($con, "aduanmpp.mysql.database.azure.com", "adminkacak@aduanmpp", {your_password}, {your_database}, 3306);
 ?>
