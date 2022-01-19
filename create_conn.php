7531292eeda2573d84e3d977c4f20db3b1af9e1f
aimanrozali <aimanrozali@student.usm.my>
2022-01-20 07:29:12 +0800

    updated connection

--- create_conn.php
+++ create_conn.php
@@ -2,5 +2,5 @@
 
 $con=mysqli_init(); 
 mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
-mysqli_real_connect($con, "aduanmpp.mysql.database.azure.com", "adminkacak@aduanmpp", {your_password}, {your_database}, 3306);
+mysqli_real_connect($con, "aduanmpp.mysql.database.azure.com", "adminkacak@aduanmpp", "Kacak_gagah01", "aduan", 3306);
 ?>
