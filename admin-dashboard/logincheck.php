97f642404d45130ba473fda043efe848db85a1f2
aimanrozali <aimanrozali@student.usm.my>
2022-02-24 16:17:57 +0800

    Update

--- admin-dashboard/logincheck.php
+++ admin-dashboard/logincheck.php
@@ -8,15 +8,17 @@ if(isset($_POST['submit'])){
 	$username = $_POST['user'];
 	$password = $_POST['pass'];
 
-	$sql = "select * from  adminlogin where admin_name='$username' and admin_pass='$password' ";
+	$sql = "SELECT * FROM  adminlogin WHERE admin_name='$username' AND admin_pass='$password'";
 	$query = mysqli_query($conn,$sql);
 
 	$row = mysqli_num_rows($query);
 		if($row == 1){
 			$_SESSION['success'] = 1;
 			header('location:dashboard.php');
+			exit();
 		}else{
 			echo "<script>alert('Wrong Credentials!'); window.location.href='index.php';</script>";
+			exit();
 		}
 
 }
