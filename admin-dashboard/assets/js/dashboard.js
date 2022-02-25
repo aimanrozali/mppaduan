6b8931b1656405a41122ddb671408597e52f8e6b
aimanrozali <aimanrozali@student.usm.my>
2022-02-26 03:36:12 +0800

    Update Report Fetch

--- admin-dashboard/assets/js/dashboard.js
+++ admin-dashboard/assets/js/dashboard.js
@@ -2,8 +2,8 @@
 
 (function ($) {
 
-  var x = parseInt(document.getElementById('x'));
-  var y = parseInt(document.getElementById('y'));
+  var x = document.getElementById('x').value;
+  var y = document.getElementById('y').value;
   console.log(x);
   console.log(y);
 
