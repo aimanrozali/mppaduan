6ff4c1a34e198e52a388307ea9e59ba2d1c6ff2d
aimanrozali <aimanrozali@student.usm.my>
2022-03-30 04:55:02 +0800

    footer optimization

--- admin-dashboard/dashboard.php
+++ admin-dashboard/dashboard.php
@@ -218,12 +218,7 @@ if(!isset($_SESSION['success'])){
          
           <!-- content-wrapper ends -->
           <!-- partial:partials/_footer.html -->
-          <footer class="footer">
-            <div class="d-sm-flex justify-content-center justify-content-sm-between">
-              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © mppusm.usm.my 2020</span>
-              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><a href="http://www.usm.my/" target="_blank">Universiti Sains Malaysia</a></span>
-            </div>
-          </footer>
+          <?php include "footer.php"; ?>
           <!-- partial -->
         </div>
         <!-- main-panel ends -->
